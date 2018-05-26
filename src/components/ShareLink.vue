9087c7dcb89a43b05eee7d30049a029632f1ac28
foxsugar <redalert3@126.com>
2018-05-26 17:26:04 +0800

    commit

--- src/components/ShareLink.vue
+++ src/components/ShareLink.vue
@@ -21,10 +21,10 @@
         console.log(response)
         wx.config({
           debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          appId: response.data.appId, // 必填，公众号的唯一标识
-          timestamp: response.data.timestamp, // 必填，生成签名的时间戳
-          nonceStr: response.data.nonceStr, // 必填，生成签名的随机串
-          signature: response.data.signature,// 必填，签名，见附录1
+          appId: response.appId, // 必填，公众号的唯一标识
+          timestamp: response.timestamp, // 必填，生成签名的时间戳
+          nonceStr: response.nonceStr, // 必填，生成签名的随机串
+          signature: response.signature,// 必填，签名，见附录1
           jsApiList: [
             'checkJsApi',
             'onMenuShareTimeline',
@@ -74,17 +74,17 @@
       let info = getCookie(infoKey)
 
       //去授权
-//      if (!info) {
-//        console.log("授权----------------")
-//        let state = id
-//        let url = 'http://tfdg38.natappfree.cc/game/wechat/authorize_base?returnUrl=' + id
-//        //跳转
-//        window.top.location.href = url
-//      } else {
-//        //加载
-//        info = decodeURIComponent(info)
-//        console.log(info)
-//      }
+      if (!info) {
+        console.log("授权----------------")
+        let state = id
+        let url = 'http://dongxiaojieqipai.com/game/wechat/authorize_base?returnUrl=' + id
+        //跳转
+        window.top.location.href = url
+      } else {
+        //加载
+        info = decodeURIComponent(info)
+        console.log(info)
+      }
     },
     methods: {}
   }
