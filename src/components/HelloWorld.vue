afb5fda8bcb528337791f878dec0980aa38f65b6
zhaobb <zhaobb@rihuisoft.com>
2018-06-03 22:35:56 +0800

    更新主页面数据

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,19 @@
 <template>
   <div class="index">
-    <!--<mt-header title="主页" >-->
-      <!--<router-link to="/" slot="left">-->
-      <!--</router-link>-->
-    <!--</mt-header>-->
-    <mt-header title="vue后台管理系统">
+    <mt-header title="董小姐棋牌室">
       <router-link to="/" slot="left">
       </router-link>
     </mt-header>
     <div class="page-content" >
-      <div class="page-title">
-        <!--<span>我的邀请码：{{this.agentId}}</span>-->
-      </div>
 
-      <div @click="handleClose">
+      <mt-cell
+        title="看邀请码"
+        is-link>
+        <img slot="icon" src="../static/img/callme.png" width="24" height="24">
+        <br>
+        <span>{{this.agentId}}</span>
 
-        <mt-cell
-          title="看邀请码"
-          is-link>
-          <!--<span style="font-size:20px;text-align:center;margin-right:150px">¥ {{this.charge}}</span>-->
-          <!--<img slot="icon" src="../assets/logo.png" width="24" height="24">-->
-          <img slot="icon" src="../static/img/callme.png" width="24" height="24">
-          <br>
-          <span>{{this.agentId}}</span>
-
-        </mt-cell>
-
-      </div>
+      </mt-cell>
 
       <mt-cell
         title="今日充值"
@@ -36,16 +23,14 @@
         <img slot="icon" src="../assets/logo.png" width="24" height="24">
       </mt-cell>
 
-      <div >
-        <mt-cell
-          title="可结算额"
-          to="/canBalance"
-          is-link>
-          <span>¥ {{this.money}}</span>
-          <!--<span style="color: gray;font-size:20px;text-align:center;margin-right:150px">¥ {{this.money}}</span>-->
-          <img slot="icon" src="../static/img/goodShow.png" width="24" height="24">
-        </mt-cell>
-      </div>
+      <mt-cell
+        title="可结算额"
+        to="/canBalance"
+        is-link>
+        <span>¥ {{this.money}}</span>
+        <!--<span style="color: gray;font-size:20px;text-align:center;margin-right:150px">¥ {{this.money}}</span>-->
+        <img slot="icon" src="../static/img/goodShow.png" width="24" height="24">
+      </mt-cell>
 
 
       <mt-cell
@@ -54,13 +39,6 @@
         is-link>
         <img slot="icon" src="../assets/logo.png" width="24" height="24">
       </mt-cell>
-
-      <!--<mt-cell-->
-        <!--title="成为代理"-->
-        <!--to="/becomeDelegate"-->
-        <!--is-link>-->
-        <!--<img slot="icon" src="../assets/logo.png" width="24" height="24">-->
-      <!--</mt-cell>-->
 
       <mt-cell
         title="关系查询"
@@ -77,57 +55,64 @@
       </mt-cell>
     </div>
 
-    <!--</div>-->
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+  import {fetchLevel2Delegate} from "../api/delegateRel";
   import { Toast } from 'mint-ui';
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      agentId:1000,
-      charge:1000,
-      money:2222.00
-    }
+  export default {
+    name: 'page-navbar',
 
-  },
-  methods:{
+    data(){
+      return {
+        pageIndex:1,//页码
+        comments: [],//商品列表数据
+        allLoaded:false, //是否禁止触发上拉函数
+        isAutoFill:false,//是否自动触发上拉函数
+        selected: '1',
+        agentId: '',
+        money: '',
+        charge: ''
 
-    blanceClick(id) {
-      this.$router.push({
-        name: 'canBlance',
-        params: {
-          id: this.money
-        }
-      })
+      }
     },
-    handleClose(){
-      Toast('邀请码是:'+ this.agentId);
+    watch:{
+      selected:{
+        handler:function(val,oldval){
+          // console.log(val)
+
+          if (val == '1'){
+            this.getPlayerList()
+          } else {
+            this.getLevel2List()
+          }
+        },
+        deep:true//对象内部的属性监听，也叫深度监听
+      },
     },
-    create(){
+
+    created() {
+      this.getHome()
+    },
+    methods: {
+      getHome() {
+        axios.get("http://localhost:8085//home/show").then((response) => {
+            console.log(response);
+            this.agentId = response['data']['data']['result']['invitationCode']
+            this.charge = response['data']['data']['result']['rebate']
+            this.money = response['data']['data']['result']['totalMoney']
+          }
+        ).catch((err) => {
+            console.log(err);
+          }
+        );
+      },
     }
-  }
-}
+  };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-/*h1, h2 {*/
-  /*font-weight: normal;*/
-/*}*/
-/*ul {*/
-  /*list-style-type: none;*/
-  /*padding: 0;*/
-/*}*/
-/*li {*/
-  /*display: inline-block;*/
-  /*margin: 0 10px;*/
-/*}*/
-/*a {*/
-  /*color: #42b983;*/
-/*}*/
 </style>
