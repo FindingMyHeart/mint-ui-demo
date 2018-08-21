e650b55b0444f3bd00b7223cfe080d91e4d518e1
zhaobb <zhaobb@rihuisoft.com>
2018-08-21 14:48:23 +0800

    a

--- src/components/TodayCharge.vue
+++ src/components/TodayCharge.vue
@@ -12,7 +12,7 @@
     </mt-header>
     <br>
     <div style="text-align: center">
-      <span style="font-size: 22px"> ￥{{this.total}} <br>{{this.lastStartDate}} 至 {{this.lastEndDate}}
+      <span style="font-size: 22px"> ￥{{this.total.toFixed(2)}} <br>{{this.lastStartDate}} 至 {{this.lastEndDate}}
         <br>
         收益￥{{this.income.toFixed(2)}}
       </span>
@@ -327,7 +327,7 @@
               this.list1 = response.result.list1;
               this.list2 = response.result.list2;
               this.list3 = response.result.list3;
-              this.income = response.result.income
+              this.income = response.result.income;
 
             });
 
--- src/components/TodayCost.vue
+++ src/components/TodayCost.vue
@@ -12,7 +12,7 @@
     <div style="text-align: center">
       <span style="font-size: 22px"> ￥{{this.total.toFixed(2)}} <br>{{this.lastStartDate}} 至 {{this.lastEndDate}}
         <br>
-        收益￥{{this.income.toFixed(2}}
+        收益￥{{this.income.toFixed(2)}}
       </span>
     </div>
     <br>
