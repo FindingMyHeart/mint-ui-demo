c6babce209df1308aef882b6f7d35a343e140495
foxsugar <redalert3@126.com>
2018-08-30 21:49:36 +0800

    commit

--- src/components/ShareLink.vue
+++ src/components/ShareLink.vue
@@ -104,7 +104,7 @@
 //          alert('去授权')
         console.log("授权----------------")
         let state = id
-        qrd = getAgentQr(id).then(response=>{
+        getAgentQr(id).then(response=>{
           this.icon = response.icon;
 
           this.qr = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=" + response.qr;
@@ -132,7 +132,7 @@
         jsapiparam(url).then(response => {
           console.log(response)
           wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: response.appId, // 必填，公众号的唯一标识
             timestamp: response.timestamp, // 必填，生成签名的时间戳
             nonceStr: response.nonceStr, // 必填，生成签名的随机串
